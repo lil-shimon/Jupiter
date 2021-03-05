6960b2bf453d20fbe316429be74c454e34ea4262
shimosawa <shimosawa@questar.ac>
2021-03-05 21:30:36 +0900

    文章データをリスト化

--- src/prepare.py
+++ src/prepare.py
@@ -9,6 +9,13 @@ with open("wagahaiwa_nekodearu.txt", mode="r", encoding="utf-8") as f:
 
 wagahai = re.sub("《[^》 ]+》", "", wagahai_original)  # ルビを取り除く
 wagahai = re.sub("[[^] ]", "", wagahai)  # 読みの注意の削除
-wagahai = re.sub("[| 　「」] ¥n", "", wagahai) # 全角、半角スペースの削除、 「」と改行の削除
+wagahai = re.sub("[| 　「」] ¥n", "", wagahai)  # 全角、半角スペースの削除、 「」と改行の削除
 
-print(wagahai)
+# print(wagahai)
+
+separator = "。"  # 。をセパレーターにする
+wagahai_list = wagahai.split(separator)  # セパレーターで文章を分けて、リストに入れる
+wagahai_list.pop()  # 最後の要素は空白になるので削除
+wagahai_list = [x + separator for x in wagahai_list]  # 文章の最後に。を追加
+
+print(wagahai_list)

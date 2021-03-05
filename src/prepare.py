d4bd2e24b605e2e3f7c48d6cf77bfbf96df4478a
shimosawa <shimosawa@questar.ac>
2021-03-05 21:25:39 +0900

    テキストデータの整形

--- src/prepare.py
+++ src/prepare.py
@@ -5,5 +5,10 @@ import re
 with open("wagahaiwa_nekodearu.txt", mode="r", encoding="utf-8") as f:
     wagahai_original = f.read()
 
-print(wagahai_original)
+# print(wagahai_original)
 
+wagahai = re.sub("《[^》 ]+》", "", wagahai_original)  # ルビを取り除く
+wagahai = re.sub("[[^] ]", "", wagahai)  # 読みの注意の削除
+wagahai = re.sub("[| 　「」] ¥n", "", wagahai) # 全角、半角スペースの削除、 「」と改行の削除
+
+print(wagahai)
